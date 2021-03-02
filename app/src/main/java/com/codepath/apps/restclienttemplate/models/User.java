bf75448ad70cdbcf1fa0f3c0e900055a9018b023
mrmikeyc <michaelchan2018@gmail.com>
2021-03-02 09:46:48 -0500

    Initial part-way commit for unit 1 of SimpleTweet

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,25 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class User {
+
+    private static final String TAG = User.class.getSimpleName();
+
+    public String name;
+    public String screenName;
+    public String profileImageURL;
+
+    public static User fromJson(JSONObject jsonObject) throws JSONException {
+        User user = new User();
+
+        user.name = jsonObject.getString("name");
+        user.screenName = jsonObject.getString("screen_name");
+        user.profileImageURL = jsonObject.getString("profile_image_url_https");
+
+        return user;
+    }
+}
